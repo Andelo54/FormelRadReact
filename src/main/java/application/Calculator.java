af9d5d7619c7088acb86b2bd898297b5632ec887
David <david.kresic@swisscom.com>
2021-11-22 23:09:52 +0100

    feat(u-from-p-and-r) method to calculate u from p and r added

--- src/main/java/application/Calculator.java
+++ src/main/java/application/Calculator.java
@@ -103,4 +103,9 @@ public class Calculator {
     public  double uFromPandI(double p,double i){
         return p / i;
     }
+
+    public  double uFromPandR(double p,double r){
+        double val = p * r;
+        return  Math.sqrt(val);
+    }
 }
